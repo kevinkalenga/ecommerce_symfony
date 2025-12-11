d64f4c8e05b793d21f0d55d44816c9ce0ea5e97c
kevin <kevinkalenga10@gmail.com>
2025-12-11 02:24:19 +0100

    Sauvegarde les informations du formulaire en bdd

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Form\RegisterUserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 final class RegisterController extends AbstractController
 {
     #[Route('/inscription', name: 'app_register')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
-        $form = $this->createForm(RegisterUserType::class);
+        $user = new User();
+      
+        $form = $this->createForm(RegisterUserType::class, $user);
+        
+        // Ecoute la request pour ecouter ce que le user va envoyer dans les champs
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+        }
+
 
         return $this->render('register/index.html.twig', [
             'registerForm' => $form->createView()
